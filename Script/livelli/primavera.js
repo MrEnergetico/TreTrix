f7dbf9ebd16ca5c092a57eb583f2fe52862aaf81
MrEnergetico <114943713+MrEnergetico@users.noreply.github.com>
2022-11-30 11:50:08 +0100

    1.4
    Aggiunti i livelli : inverno :snowflake: , estate :sunny: , autunno :fallen_leaf: e primavera :sunflower:

---
+++ Script/livelli/primavera.js
@@ -0,0 +1,118 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const tiles = Array.from(document.querySelectorAll('.casella'));
+    const playerDisplay = document.querySelector('.display-player');
+    const resetButton = document.querySelector('#reset');
+    const announcer = document.querySelector('.announcer');
+
+    let board = ['', '', '', '', '', '', '', '', ''];
+    let currentPlayer = '🌹';
+    let isGameActive = true;
+
+    const PLAYER_X_WON = 'PLAYER_X_WON';
+    const PLAYER_O_WON = 'PLAYER_O_WON';
+    const PAREGGIO = 'PAREGGIO';
+
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    function handleResultValidation() {
+        let roundWon = false;
+        for (let i = 0; i <= 7; i++) {
+            const winCondition = winningConditions[i];
+            const a = board[winCondition[0]];
+            const b = board[winCondition[1]];
+            const c = board[winCondition[2]];
+            if (a === '' || b === '' || c === '') {
+                continue;
+            }
+            if (a === b && b === c) {
+                roundWon = true;
+                break;
+            }
+        }
+
+    if (roundWon) {
+            announce(currentPlayer === '🌹' ? PLAYER_X_WON : PLAYER_O_WON);
+            isGameActive = false;
+            return;
+        }
+
+    if (!board.includes(''))
+        announce(PAREGGIO);
+    }
+
+    const announce = (type) => {
+        switch(type){
+            case PLAYER_O_WON:
+                announcer.innerHTML = 'IL GIOCATORE <span class="playerO">🌿</span> HA VINTO !!';
+                break;
+            case PLAYER_X_WON:
+                announcer.innerHTML = 'IL GIOCATORE <span class="playerX">🌹</span> HA VINTO';
+                break;
+            case PAREGGIO:
+                announcer.innerText = 'NESSUN GIOCATORE HA VINTO !!';
+        }
+        announcer.classList.remove('hide');
+    };
+
+    const isValidAction = (tile) => {
+        if (tile.innerText === '🌹' || tile.innerText === '🌿'){
+            return false;
+        }
+
+        return true;
+    };
+
+    const updateBoard =  (index) => {
+        board[index] = currentPlayer;
+    }
+
+    const changePlayer = () => {
+        playerDisplay.classList.remove(`player${currentPlayer}`);
+        currentPlayer = currentPlayer === '🌹' ? '🌿' : '🌹'
+        playerDisplay.innerText = currentPlayer;
+        playerDisplay.classList.add(`player${currentPlayer}`);
+    }
+
+    const userAction = (tile, index) => {
+        if(isValidAction(tile) && isGameActive) {
+            tile.innerText = currentPlayer;
+            tile.classList.add(`player${currentPlayer}`);
+            updateBoard(index);
+            handleResultValidation();
+            changePlayer();
+        }
+    }
+    
+    const resetBoard = () => {
+        board = ['', '', '', '', '', '', '', '', ''];
+        isGameActive = true;
+        announcer.classList.add('hide');
+
+        if (currentPlayer === '🌿') {
+            changePlayer();
+        }
+
+        tiles.forEach(tile => {
+            tile.innerText = '';
+            tile.classList.remove('playerX');
+            tile.classList.remove('playerO');
+        });
+    }
+
+    tiles.forEach( (tile, index) => {
+        tile.addEventListener('click', () => userAction(tile, index));
+    });
+
+    resetButton.addEventListener('click', resetBoard);
+});
+
+
